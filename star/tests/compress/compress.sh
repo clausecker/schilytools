d8df195c6b86f22890b5062b3fb916b246e901ce
Robert Clausecker <fuz@fuz.su>
2022-09-29 16:42:32 +0200

    star/tests/compress: rename test archives to avoid case problems
    Case-sensitive file systems such as NTFS and Apple HFS don't like
    seeing two files whose names only differ in letter case.  Fix this
    problem by giving the test files truly distinct names.
    
    Reported by:	tux <rosa.elefant@icloud.com>
    Fixes		#28

--- star/tests/compress/compress.sh
+++ star/tests/compress/compress.sh
@@ -15,28 +15,28 @@ LC_ALL=C export LC_ALL
 
 # File name	COMPR.		Created with:	Uncompress with:
 #
-# tar.tar.z	1 C_PACK	"pack"		"pack" / "gzip"
-# tar.tar.gz	2 C_GZIP	"gzip"		"gzip"
-# tar.tar.Z	3 C_LZW		"compress"	"uncompress" / "gzip"
-#		4 C_FREEZE	"freeze"	"melt" / "gzip"
-#		5 C_LZH		"lzh"		"gzip"
-#		6 C_PKZIP	"pkzip"		"gzip"
-# tar.tar.bz2	7 C_BZIP2	"bzip2"		"bzip2"
-# tar.tar.lzo	8 C_LZO		"lzop"		"lzop"
-# tar.tar.7z	9 C_7Z		"p7zip"		"p7zip"
-# tar.tar.xz	10 C_XZ		"xz"		"xz"
-# tar.tar.lz	11 C_LZIP	"lzip"		"lzip"
-# tar.tar.zst	12 C_ZSTD	"zstd"		"zstd"
-# tar.tar.lzma	13 C_LZMA	"lzma"		"lzma"
-# tar.tar.F2	14 C_FREEZE2	"freeze"	"freeze"
+# pack.tar.z	 1 C_PACK	"pack"		"pack" / "gzip"
+# gzip.tar.gz	 2 C_GZIP	"gzip"		"gzip"
+# compress.tar.Z 3 C_LZW	"compress"	"uncompress" / "gzip"
+#		 4 C_FREEZE	"freeze"	"melt" / "gzip"
+#		 5 C_LZH	"lzh"		"gzip"
+#		 6 C_PKZIP	"pkzip"		"gzip"
+# bzip2.tar.bz2	 7 C_BZIP2	"bzip2"		"bzip2"
+# lzo.tar.lzo	 8 C_LZO	"lzop"		"lzop"
+# 7z.tar.7z	 9 C_7Z		"p7zip"		"p7zip"
+# xz.tar.xz	 10 C_XZ	"xz"		"xz"
+# lzip.tar.lz	 11 C_LZIP	"lzip"		"lzip"
+# zstd.tar.zst	 12 C_ZSTD	"zstd"		"zstd"
+# lzma.tar.lzma	 13 C_LZMA	"lzma"		"lzma"
+# freeze2.tar.F2 14 C_FREEZE2	"freeze"	"freeze"
 
 #
 # pack	1 C_PACK
 #
 if type gzip > /dev/null; then		# XXX See gzip related "fi" below
 
-if gzip -d < tar.tar.z > /dev/null; then
-s=tar.tar.z
+if gzip -d < pack.tar.z > /dev/null; then
+s=pack.tar.z
 docommand COMP-pack "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'pack' compressed, trying to use the -z option.
@@ -44,7 +44,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-pack-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.z: pack compressed ustar archive.
+pack.tar.z: pack compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -58,7 +58,7 @@ fi
 #
 # gzip	2 C_GZIP
 #
-s=tar.tar.gz
+s=gzip.tar.gz
 docommand COMP-gzip "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'gzip' compressed, trying to use the -z option.
@@ -66,7 +66,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-gzip-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.gz: gzip compressed ustar archive.
+gzip.tar.gz: gzip compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -74,8 +74,8 @@ star: Blocksize = 3 records.
 #
 # lzw	3 C_LZW	(compress)
 #
-if gzip -d < tar.tar.Z > /dev/null; then
-s=tar.tar.Z
+if gzip -d < compress.tar.Z > /dev/null; then
+s=compress.tar.Z
 docommand COMP-lzw-compress "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'lzw' compressed, trying to use the -z option.
@@ -83,7 +83,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-lzw-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.Z: lzw compressed ustar archive.
+compress.tar.Z: lzw compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -113,7 +113,7 @@ fi				# XXX This is the gzip related "fi"
 # bzip2	7 C_BZIP2
 #
 if type bzip2 > /dev/null; then
-s=tar.tar.bz2
+s=bzip2.tar.bz2
 docommand COMP-bzip2 "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'bzip2' compressed, trying to use the -bz option.
@@ -121,7 +121,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-bzip2-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.bz2: bzip2 compressed ustar archive.
+bzip2.tar.bz2: bzip2 compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -133,7 +133,7 @@ fi
 # lzo	8 C_LZO
 #
 if type lzop > /dev/null; then
-s=tar.tar.lzo
+s=lzo.tar.lzo
 docommand COMP-lzo "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'lzop' compressed, trying to use the -lzo option.
@@ -141,7 +141,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-lzo-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.lzo: lzo compressed ustar archive.
+lzo.tar.lzo: lzo compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -153,7 +153,7 @@ fi
 # 7z	9 C_7Z
 #
 if type p7zip > /dev/null; then
-s=tar.tar.7z
+s=7z.tar.7z
 docommand COMP-7z "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is '7z' compressed, trying to use the -7z option.
@@ -161,7 +161,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-7z-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.7z: 7z compressed ustar archive.
+7z.tar.7z: 7z compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -173,7 +173,7 @@ fi
 # xz	10 C_XZ
 #
 if type xz > /dev/null; then
-s=tar.tar.xz
+s=xz.tar.xz
 docommand COMP-xz "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'xz' compressed, trying to use the -xz option.
@@ -181,7 +181,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-xz-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.xz: xz compressed ustar archive.
+xz.tar.xz: xz compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -193,7 +193,7 @@ fi
 # lzip	11 C_LZIP
 #
 if type lzip > /dev/null; then
-s=tar.tar.lz
+s=lzip.tar.lz
 docommand COMP-lzip "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'lzip' compressed, trying to use the -lzip option.
@@ -201,7 +201,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-lzip-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.lz: lzip compressed ustar archive.
+lzip.tar.lz: lzip compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -213,7 +213,7 @@ fi
 # zstd	12 C_ZSTD
 #
 if type zstd > /dev/null; then
-s=tar.tar.zst
+s=zstd.tar.zst
 docommand COMP-zstd "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'zstd' compressed, trying to use the -zstd option.
@@ -221,7 +221,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-zstd-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.zst: zstd compressed ustar archive.
+zstd.tar.zst: zstd compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -233,7 +233,7 @@ fi
 # lzma	13 C_LZMA
 #
 if type lzma > /dev/null; then
-s=tar.tar.lzma
+s=lzma.tar.lzma
 docommand COMP-lzma "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'lzma' compressed, trying to use the -lzma option.
@@ -241,7 +241,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-lzma-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.lzma: lzma compressed ustar archive.
+lzma.tar.lzma: lzma compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
@@ -253,7 +253,7 @@ fi
 # lzma	14 C_FREEZE2
 #
 if type freeze > /dev/null; then
-s=tar.tar.F2
+s=freeze2.tar.F2
 docommand COMP-freeze2 "${tar} -t f=${s}" 0 "file
 " "\
 star: WARNING: Archive is 'freeze2' compressed, trying to use the -freeze option.
@@ -261,7 +261,7 @@ star: Blocksize = 3 records.
 star: 1 blocks + 0 bytes (total of 1536 bytes = 1.50k).
 "
 docommand COMP-freeze2-prtype "${tar} -t -print-artype f=${s}" 0 "\
-tar.tar.F2: freeze2 compressed ustar archive.
+freeze2.tar.F2: freeze2 compressed ustar archive.
 " "\
 star: Blocksize = 3 records.
 "
