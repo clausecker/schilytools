4feb987c8a3ac6bbab380818f2ac536addec29d5
Robert Clausecker <fuz@fuz.su>
2023-11-22 01:17:30 -0500

    smake/version.h: bump version date following f67e8c4e239d
    Forgot to do this previously.
    
    Reported-by:	Caleb Xu
    Fixes:		#61

--- smake/version.h
+++ smake/version.h
@@ -1,7 +1,11 @@
 /* @(#)version.h	1.12 21/08/12 Copyright 2018-2021 J. Schilling */
 
 /*
+ * Copyright 2023 the schilytools team
+ */
+
+/*
  * The version for smake
  */
 #define	VERSION		"1.7"
-#define	VERSION_DATE	"2022/11/27"
+#define	VERSION_DATE	"2023/11/22"
